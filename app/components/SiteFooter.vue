690e86c0d39632ff695a6b5b3d1139160771743a
guangzhoueven <10062132@qq.com>
2025-10-11 23:04:42 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/guangzhoueven",
+  home: "https://home.guangzhoueven.dpdns.org",
+  email: "mailto:10062132@qq.com",
 };
 </script>
 
